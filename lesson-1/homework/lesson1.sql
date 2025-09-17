2608a4cfdd824e9808319846ad7853c0c6b6f9e4
BunyodMirzaev <bunyod.mirzaev@gmail.com>
2025-09-18 00:57:19 +0500

    homework-1.sql

--- lesson-1/homework/lesson1.sql
+++ lesson-1/homework/lesson1.sql
@@ -1,1 +1,20 @@
-bunyod mirzaev
+Easy
+1. (Data)Ma’lumot — bu raqamlar, matnlar, suratlar yoki tovush kabi xom (ishlanmagan) fakt va raqamlar bo‘lib, ularni tahlil qilish yoki qayta ishlash mumkin. 
+(Database)Ma’lumotlar bazasi — bu ma’lumotlarni saqlash va boshqarish uchun mo‘ljallangan tizimli va elektron shakldagi ombor.U orqali ma’lumotlarni oson qidirish, kiritish va tahrirlash mumkin.
+(Relational Database)Relatsion ma’lumotlar bazasi — bu ma’lumotlarni jadval (table) ko‘rinishida saqlaydigan va ular orasidagi bog‘lanishlarni (relatsiyalarni) qo‘llab-quvvatlaydigan ma’lumotlar bazasi. Bu turdagi bazalarda asosan SQL tili ishlatiladi.
+(Table)Jadval — bu ma’lumotlar bazasidagi asosiy tuzilma bo‘lib, qatorlar (row) va ustunlar (column) shaklida tashkil etiladi. Har bir qator — bitta yozuv (record), har bir ustun esa — ma’lumot turi (masalan, ism, yosh) ni bildiradi.
+
+2. List five key features of SQL Server.
+(High Availability & Disaster Recovery)
+(Security)
+(Advanced Querying)
+(Business Intelligence)
+(Performance & Scalability)
+
+3.What are the different authentication modes available when connecting to SQL Server? (Give at least 2)
+Windows autentifikatsiyasi
+SQL Server autentifikatsiyasi
+
+Medium
+Create database SchoolDB
+go
